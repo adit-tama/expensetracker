02ec90da2291d5234835ee47bc66a6b1453d76c8
Praditya Adhitama <pradityadhitama@gmail.com>
2024-12-26 12:04:55 +0100

    clean up

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabaseAuthClient } from "@/utils/supabase/server";
 import { COOKIES_NAMES } from "../../utils/constants";
-import Cookies from "cookies";
+import { serialize } from "cookie";
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { data, error } = await supabaseAuthClient.auth.signInWithPassword(
@@ -15,14 +15,18 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     return;
   }
 
-  const cookies = new Cookies(req, res);
+  const cookie = serialize(
+    COOKIES_NAMES.ACCESS_TOKEN,
+    data.session.access_token,
+    {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7, // One week
+      path: "/",
+    }
+  );
 
-  cookies.set(COOKIES_NAMES.ACCESS_TOKEN, data.session.access_token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    path: "/",
-    sameSite: "strict",
-  });
+  res.setHeader("Set-Cookie", cookie);
 
   res.status(200).json({
     message: "Login successful",
